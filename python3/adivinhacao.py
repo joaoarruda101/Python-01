a84340916787ea979fc30ede5f6ee49be0fd5006
joao.victor <victorfurquim00@gmail.com>
2023-02-27 11:19:16 -0300

    novas-condicoes-&-variaveis

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -7,15 +7,22 @@ chute_str = input("Digite o seu número: ")
 print("Você digitou" , chute_str)
 
 chute = int(chute_str)
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-if(chute_str == numero_secreto):
+if(acertou):
    print("Você acertou")
 else:
-   print("Você errou")
-
+   if(maior):
+    print("Você errou! O seu chute foi *MAIOR* que o número secreto!")
+   elif(menor):
+      print("Você errou! o seu chute foi *MENOR* do que o número secreto!")
 print("FIM DO JOGO");
 
 idade1 = 10;
 idade2 = int("20");
 print(idade1 + idade2)
+
+
 
